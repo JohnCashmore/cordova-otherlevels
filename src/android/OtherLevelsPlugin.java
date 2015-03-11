48902c1558530540e99a8b82b76aad8cbf6772ab
John Cashmore <cashy@whoooop.co.uk>
2015-03-11 14:47:01 +0000

    Initial commit

---
+++ src/android/OtherLevelsPlugin.java
@@ -0,0 +1,46 @@
+package com.nudge.judopay;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class OtherLevelsPlugin extends CordovaPlugin {
+   
+
+    private CallbackContext callbackContext;
+
+    @Override
+    public boolean execute(String action, JSONArray args,
+            CallbackContext callbackContext) throws JSONException {
+
+        //setting callback context
+        this.callbackContext = callbackContext;
+
+        if (action.equals("makeSimpleTransaction")){
+                /*this.makePayment(this.cordova.getActivity().getApplicationContext(), args.getString(0), MY_JUDO_ID,
+                    MY_API_TOKEN, MY_API_SECRET, "ref68798", "cref43434",
+                    null, null, false,
+                    SERVER);*/
+
+            // this.makeOauthPayment(this.cordova.getActivity().getApplicationContext(),
+            //                     args.getString(0), // paymentAmount
+            //                     args.getString(1), // currency
+            //                     args.getString(2), // paymentRef
+            //                     args.getString(3), // consumerRef
+            //                     args.getString(4) // userReceipt
+            //                     );
+            return true;
+        }
+
+        return false;
+    }
+
+    
+}
